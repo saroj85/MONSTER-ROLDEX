2316ad4008092d7412195a6422be18453c273af4
Saroj@852131 <send4saroj@gmail.com>
2020-01-28 21:42:06 +0530

    Updates

---
+++ precache-manifest.c1efff1aee825f9d133fe9e27fbf197d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f0a83290d574978f25ff11d7229f54",
+    "url": "/MONSTER-ROLDEX/index.html"
+  },
+  {
+    "revision": "f6ae31216cc9d06cc32a",
+    "url": "/MONSTER-ROLDEX/static/css/main.675a18f5.chunk.css"
+  },
+  {
+    "revision": "ee113db423c32257aaf3",
+    "url": "/MONSTER-ROLDEX/static/js/2.037f21a6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MONSTER-ROLDEX/static/js/2.037f21a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6ae31216cc9d06cc32a",
+    "url": "/MONSTER-ROLDEX/static/js/main.b3bcfecf.chunk.js"
+  },
+  {
+    "revision": "e88e59aadf02ff1c68af",
+    "url": "/MONSTER-ROLDEX/static/js/runtime-main.65ff4169.js"
+  }
+]);
